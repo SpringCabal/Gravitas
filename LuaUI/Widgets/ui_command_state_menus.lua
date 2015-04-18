0c4a856c8e046c7a31ca60bb21d9a0360d6bf234
Gajo Petrovic <gajo01@gmail.com>
2015-04-18 15:46:44 +0900

    make the player character hold fire by default

--- LuaUI/Widgets/ui_command_state_menus.lua
+++ LuaUI/Widgets/ui_command_state_menus.lua
@@ -469,3 +469,11 @@ end
 function widget:Shutdown()
 	spSendCommands({'tooltip 1'})
 end
+
+function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+    local HOLD_FIRE = 2
+    if UnitDefs[unitDefID].customParams.player and unitTeam == Spring.GetMyTeamID() then
+        Spring.GiveOrderToUnit(unitID, CMD.INSERT, 
+            { 0, CMD.FIRE_STATE, 0, HOLD_FIRE},{"alt"})
+    end
+end
