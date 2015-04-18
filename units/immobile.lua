234ecc52bfed38955e2e75b4bb951b9d1948d8d4
Gajo Petrovic <gajo01@gmail.com>
2015-04-18 17:16:25 +0900

    added fire unit

--- units/immobile.lua
+++ units/immobile.lua
@@ -6,7 +6,16 @@ local Barrel = BaseImmobile:New {
     maxDamage           = 100,
 }
 
+local Fire = BaseImmobile:New {
+    name                = "Fire",
+    canMove             = false, --fire cannot be moved (even by gravity)
+    customParams = {
+        fire = true,
+    },
+    maxDamage           = 100,
+}
 
 return lowerkeys({
     Barrel = Barrel,
+    Fire   = Fire,
 })
