68d2901c039e3e983759d6f39fd653e4e0045a92
silentwings <nicfreeman1209@gmail.com>
2015-04-18 14:10:47 +0100

    add shield weapon to electrafi (-> the unit needs to have a weapon set up in its model/script)

--- BaseClasses/Units/Immobile.lua
+++ BaseClasses/Units/Immobile.lua
@@ -12,11 +12,11 @@ local BaseImmobile = Unit:New {
     collisionVolumeScales   = '37 40 37',
     collisionVolumeTest     = 1,
     collisionVolumeType     = 'CylY',
-	-- corpse				= "<SIDE>soldier_dead",
+	-- corpse				= "",
 	footprintX			= 1,
 	footprintZ			= 1,
 	mass				= 50,
-	maxDamage			= 300, -- default only, <SIDE>Infantry.lua should overwrite
+	maxDamage			= 300, -- default only
 	repairable			= false,
 	sightDistance		= 250,
 
@@ -34,10 +34,7 @@ local BaseEffect = BaseImmobile:New {
     maxDamage    = 10000,
     blocking     = false,
     canMove      = false, --effects cannot be moved (even by gravity)
-    canCloak            = true,
-    initCloaked         = true,
-    decloakOnFire       = false,
-    minCloakDistance    = -1,
+    turnRate     = 0,
 }
 
 return {
