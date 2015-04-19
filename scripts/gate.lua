66d75bbbb75eefc0a902f541302add3c55f957d2
Anarchid <anarchid@mail.ru>
2015-04-19 14:59:35 +0300

    probably gate animation

--- scripts/gate.lua
+++ scripts/gate.lua
@@ -1,3 +1,6 @@
+local gate = piece('Gate')
+local rails = piece('Rails);
+
 function script.Create()
 end
 
@@ -6,9 +9,15 @@ function script.Killed(recentDamage, _)
 end
 
 function script.Activate()
+	Move(rails, z_axis, 0,10);
+	WaitForMove(gate, z_axis);
+	Move(rails, z_axis, 0, 20);
     return 1
 end
 
 function script.Deactivate()
+	Move(rails, z_axis, -160,10);
+	WaitForMove(gate, z_axis);
+	Move(rails, z_axis, -160, 20);
     return 0
 end
