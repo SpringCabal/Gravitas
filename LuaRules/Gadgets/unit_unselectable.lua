58562dc9a013b48380b0ae6568aef916a2fd0f3a
Gajo Petrovic <gajo01@gmail.com>
2015-04-20 23:33:23 +0900

    make some units (walls, plates and effects) unselectable in play mode

---
+++ LuaRules/Gadgets/unit_unselectable.lua
@@ -0,0 +1,30 @@
+function gadget:GetInfo()
+  return {
+    name      = "Unselectable objects",
+    desc      = "Unselectable objects (hidden during play mode)",
+    author    = "gajop",
+    date      = "April 2015",
+    license   = "GNU GPL, v3",
+    layer     = 0,
+    enabled   = true,
+  }
+end
+
+
+-- FlameRaw format: posx,posy,posz, dirx,diry,dirz, speedx,speedy,speedz, range
+
+-- SYNCED
+if gadgetHandler:IsSyncedCode() then
+    
+local devMode = (tonumber(Spring.GetModOptions().play_mode) or 0) == 0
+
+function gadget:UnitCreated(unitID, unitDefID)
+    if devMode then
+        local unitDef = UnitDefs[unitDefID]
+        if unitDef.customParams.wall or unitDef.customParams.effect or unitDef.name == "plate" then
+            Spring.SetUnitNoSelect(unitID, true)
+        end
+    end
+end
+
+end
