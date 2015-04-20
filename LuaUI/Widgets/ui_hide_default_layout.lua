1ef05dc2ba0fce9ad73bcf00092235ce07d6f2a6
silentwings <nicfreeman1209@gmail.com>
2015-04-20 19:41:34 +0100

    rename standalone_lobby -> hide_default_layout, clean

--- LuaUI/Widgets/ui_hide_default_layout.lua
+++ LuaUI/Widgets/ui_hide_default_layout.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
   return {
-    name      = "Standalone lobby tools",
-    desc      = "Hides the default interface",
+    name      = "Hides the default interface",
+    desc      = "",
     author    = "gajop",
     date      = "in the future",
     license   = "GPL-v2",
@@ -11,12 +11,7 @@ function widget:GetInfo()
 end
 
 function widget:Initialize()
-    -- TODO: Use this line for release (disables the console)
-    --Spring.SendCommands("ResBar 0", "ToolTip 0", "Console 0", "Clock 0", "Info 0")
-    
     -- check if scenario editor is run in play mode
-    Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0")
-    Spring.SendCommands("bind q onoff")
     local devMode = (tonumber(Spring.GetModOptions().play_mode) or 0) == 0
     if not devMode then
         Spring.SendCommands("Console 0")
@@ -24,11 +19,16 @@ function widget:Initialize()
         -- FIXME: this still doesn't disable shift+w and shift+p
         Spring.SendCommands("unbindkeyset w", "unbindkeyset p")
     end
+
+    Spring.SendCommands("bind q onoff")
+    
+    -- remove Springs default UI stuff
+    Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0")
     gl.SlaveMiniMap(true)
     gl.ConfigMiniMap(-1,-1,-1,-1)
 end
 
--- sets status to ready; avoiding any pre-game UI
+-- sets status to ready & hide pre-game UI
 function widget:GameSetup()
     return true, true
 end
