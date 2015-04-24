6b6066d21b148ef674e797357e9e1d9d7f764d24
Gajo Petrovic <gajo01@gmail.com>
2015-04-25 02:12:51 +0900

    fix #90

--- scripts/gate.lua
+++ scripts/gate.lua
@@ -8,12 +8,14 @@ function script.Killed(recentDamage, _)
     return 1
 end
 
-local signalMask = 0
+local signalMask = 1
 
 local collisionData
 -- TODO: blocking/collision will be incosistent while it's pulling down/up
 function script.Activate()
+    Spring.UnitScript.Signal(signalMask)
     StartThread(function()
+        Spring.UnitScript.SetSignalMask(signalMask)
         GG.AdjustWallTerrain(unitID, 0)
         Sleep(500)
         local x, y, z = Spring.GetUnitPosition(unitID)
@@ -22,13 +24,13 @@ function script.Activate()
         WaitForMove(gate, z_axis);
         Spring.SetUnitBlocking(unitID, false, false, false, false, false, false, false)
     end)
-    Spring.UnitScript.Signal(signalMask)
-  --  Spring.UnitScript.SetSignalMask(signalMask)
     return 1
 end
 
 function script.Deactivate()
+    Spring.UnitScript.Signal(signalMask)
     StartThread(function()
+        Spring.UnitScript.SetSignalMask(signalMask)
         local x, y, z = Spring.GetUnitPosition(unitID)
         Spring.PlaySoundFile("sounds/gate.ogg", 0.7)
         Move(gate, z_axis, 0, 190);
@@ -36,7 +38,5 @@ function script.Deactivate()
         Spring.SetUnitBlocking(unitID, true)
         GG.AdjustWallTerrain(unitID, 1)
     end)
-    Spring.UnitScript.Signal(signalMask)
-  --  Spring.UnitScript.SetSignalMask(signalMask)
     return 0
 end
