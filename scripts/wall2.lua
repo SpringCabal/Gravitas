c80c4e459c47277b2cdfb1b9ddb8858667ab3355
Anarchid <anarchid@mail.ru>
2015-04-25 23:08:20 +0300

    splode wall2

--- scripts/wall2.lua
+++ scripts/wall2.lua
@@ -1,10 +1,19 @@
-local cube = piece('Cube');
+local frags = {}
+
+for i=1,11 do
+    frags[i] = piece('frag'..i);
+end
 
 function script.Create()
 end
 
 function script.Killed(recentDamage, _)
-	Explode(cube, SFX.SHATTER);
+    for i,v in ipairs(frags) do
+		local x,y,z = Spring.GetUnitPiecePosDir(unitID, v);
+		Spring.SpawnCEG("flashnuke", x,y,z, 0,0,0, 0, 0)
+		Explode(v, SFX.FALL+SFX.EXPLODE);
+    end
+    
     return 1
 end
 
